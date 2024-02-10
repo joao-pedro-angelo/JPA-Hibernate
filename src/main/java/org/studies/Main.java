3e3eb82d5767b95d99ecf1cde0772b9f8c86d33b
my-github-2024 <carneiroangelojoaopedro@gmail.com>
2024-02-10 14:47:41 -0300

    Inicia Main e Exception

--- src/main/java/org/studies/Main.java
+++ src/main/java/org/studies/Main.java
@@ -3,6 +3,7 @@ package org.studies;
 import org.studies.JPAUtil.CreateEntityManager;
 import org.studies.daos.CategoryDAO;
 import org.studies.daos.ProductDAO;
+import org.studies.exception.RegraDeNegocio;
 import org.studies.services.ServiceCategory;
 import org.studies.services.ServiceProduct;
 
@@ -17,7 +18,86 @@ public class Main {
     private static final ServiceProduct serviceProduct = new ServiceProduct(new ProductDAO(entityManager), entityManager);
 
     public static void main(String[] args) {
+        int opcao = exibirMenu();
+        while (opcao != 9){
+            try{
+                switch (opcao){
+                    case 1:
+                        criarCategoria();
+                        break;
+                    case 2:
+                        criarProduto();
+                        break;
+                    case 3:
+                        removerCategoria();
+                        break;
+                    case 4:
+                        removerProduto();
+                        break;
+                    case 5:
+                        exibirCategoria();
+                        break;
+                    case 6:
+                        exibirProduto();
+                        break;
+                    case 7:
+                        atualizarCategoria();
+                        break;
+                    case 8:
+                        atualizarProduto();
+                        break;
+                }
+            } catch (RegraDeNegocio e){
+                System.out.println("Erro: " + e.getMessage());
+            }
+            opcao = exibirMenu();
+        }
+        System.out.println("Finalizando aplicação...");
+    }
 
+    private static void atualizarProduto() {
+    }
+
+    private static void atualizarCategoria() {
+    }
+
+    private static void exibirProduto() {
+    }
+
+    private static void exibirCategoria() {
+    }
+
+    private static void removerProduto() {
+    }
+
+    private static void removerCategoria() {
+    }
+
+    private static void criarProduto() {
+    }
+
+    private static void criarCategoria() {
+    }
+
+    private static int exibirMenu(){
+        System.out.println("""
+                CLASSIC BANK - ESCOLHA UMA OPÇÃO:
+                1 - Criar categoria
+                2 - Criar produto
+                3 - Remover categoria
+                4 - Remover produto
+                5 - Exibir categoria
+                6 - Exibir produto
+                7 - Atualizar categoria
+                8 - Atualizar produto
+                9 - Sair
+                """);
+        try{
+            return Integer.parseInt(scanner.nextLine());
+        } catch (NumberFormatException e){
+            System.out.println("Entrada inválida...");
+            return 9;
+        }
     }
 
 }
---
+++ src/main/java/org/studies/exception/RegraDeNegocio.java
@@ -0,0 +1,8 @@
+package org.studies.exception;
+
+public class RegraDeNegocio extends RuntimeException {
+
+    public RegraDeNegocio(String msg){
+        super(msg);
+    }
+}
