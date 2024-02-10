2c610066ec482f114226adfc7b9d6f3b0488ad16
my-github-2024 <carneiroangelojoaopedro@gmail.com>
2024-02-10 14:37:09 -0300

    Inicia Main

--- src/main/java/org/studies/Main.java
+++ src/main/java/org/studies/Main.java
@@ -1,6 +1,20 @@
 package org.studies;
 
+import org.studies.JPAUtil.CreateEntityManager;
+import org.studies.daos.CategoryDAO;
+import org.studies.daos.ProductDAO;
+import org.studies.services.ServiceCategory;
+import org.studies.services.ServiceProduct;
+
+import javax.persistence.EntityManager;
+import java.util.Scanner;
+
 public class Main {
+
+    private static final Scanner scanner = new Scanner(System.in).useDelimiter("\n");
+    private static EntityManager entityManager = CreateEntityManager.createEntityManager();
+    private static final ServiceCategory serviceCategory = new ServiceCategory(new CategoryDAO(entityManager), entityManager);
+    private static final ServiceProduct serviceProduct = new ServiceProduct(new ProductDAO(entityManager), entityManager);
 
     public static void main(String[] args) {
 
