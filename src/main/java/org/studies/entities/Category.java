a6885575831b1a0f9033b18aa96bd8b3b962ca54
my-github-2024 <carneiroangelojoaopedro@gmail.com>
2024-02-14 14:51:10 -0300

    Category Documentação

--- src/main/java/org/studies/entities/Category.java
+++ src/main/java/org/studies/entities/Category.java
@@ -2,23 +2,47 @@ package org.studies.entities;
 
 import javax.persistence.*;
 
+/**
+ * Classe para representar uma Categoria
+ * Entidade Categoria - Representada por uma tabela na base de dados
+ *
+ * @author carneiroangelojoaopedro@gmail.com
+ */
 @Entity
 @Table(name = "CategoryTable")
 public class Category {
 
+    /**
+     * O identificador de uma categoria é o nome
+     */
     @Id
     private String name;
 
+    /**
+     * Construtor padrão da entidade
+     */
     public Category(){}
 
+    /**
+     * Construtor que recebe o nome
+     * @param name String nome da entidade
+     */
     public Category(String name){
         this.name = name;
     }
 
+    /**
+     * Método setter para o nome
+     * @param name String nome da categoria
+     */
     public void setName(String name){
         this.name = name;
     }
 
+    /**
+     * Representação textual da entidade
+     * @return Representação textual da entidade
+     */
     public String toString(){
         return "Categoria: " + this.name + "\n";
     }
