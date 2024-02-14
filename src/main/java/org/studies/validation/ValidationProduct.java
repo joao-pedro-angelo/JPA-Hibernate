b4eb5577265033d3ca94b3314ff6443e6460d1ab
my-github-2024 <carneiroangelojoaopedro@gmail.com>
2024-02-14 14:57:55 -0300

    Documentação da ValidationProduct

--- src/main/java/org/studies/validation/ValidationProduct.java
+++ src/main/java/org/studies/validation/ValidationProduct.java
@@ -1,20 +1,36 @@
 package org.studies.validation;
 
-import org.studies.entities.Category;
 import org.studies.exception.RegraDeNegocioException;
 
 import java.math.BigDecimal;
 
+/**
+ * Validação dos atributos do Produto
+ *
+ * @author carneiroangelojoaopedro@gmail.com
+ */
 public class ValidationProduct {
 
+    /**
+     * Valida nome do produto
+     * @param nameProduct nome do produto
+     */
     public static void validationName(String nameProduct){
         if (nameProduct.isBlank()) throw new RegraDeNegocioException("Nome inválido!");
     }
 
+    /**
+     * Valida valor do produto
+     * @param valueProduct valor do produto
+     */
     public static void validationValue(BigDecimal valueProduct){
         if (valueProduct == null) throw new RegraDeNegocioException("Valor inválido!");
     }
 
+    /**
+     * Valida quantidade do produto
+     * @param quantity quantidade do produto
+     */
     public static void validationQuantity(int quantity){
         if (quantity < 0) throw new RegraDeNegocioException("Quantidade inválida!");
     }
