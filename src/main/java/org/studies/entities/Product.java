238c44b80ddb43a56db7c219a40894d1a0fc78ce
my-github-2024 <carneiroangelojoaopedro@gmail.com>
2024-02-14 14:54:03 -0300

    Documentação do Product

--- src/main/java/org/studies/entities/Product.java
+++ src/main/java/org/studies/entities/Product.java
@@ -3,22 +3,59 @@ package org.studies.entities;
 import javax.persistence.*;
 import java.math.BigDecimal;
 
+/**
+ * Representação de uma entidade na base de dados como uma tabela
+ *
+ * @author carneiroangelojoaopedro@gmail.com
+ */
 @Entity
 @Table(name = "ProductTable")
 public class Product {
 
+    /**
+     * A chave da entidade produto é o nome do produto
+     */
     @Id
     private String name;
+
+    /**
+     * Coluna "quant"
+     * Quantidade do produto
+     */
     @Column(name = "quant")
     private Integer quantity;
+
+    /**
+     * Coluna "desc"
+     * Descrição do produto
+     */
     @Column(name = "desc")
     private String description;
+
+    /**
+     * Coluna "value"
+     * Valor do produto
+     */
     private BigDecimal value;
+
+    /**
+     * Coluna Category
+     * Categoria do produto
+     *
+     * Relacionamento ManyToOne
+     * Muitos produtos para uma categoria
+     */
     @ManyToOne
     private Category category;
 
+    /**
+     * Construtor padrão - default
+     */
     public Product() {}
 
+    /**
+     * Construtor all-args
+     */
     public Product(String name, Integer quantity, String description, BigDecimal value, Category category) {
         this.name = name;
         this.quantity = quantity;
@@ -27,6 +64,7 @@ public class Product {
         this.category = category;
     }
 
+    // Setters e getters abaixo
     public void setName(String name){
         this.name = name;
     }
@@ -35,18 +73,14 @@ public class Product {
         this.quantity = quantity;
     }
 
-    public void setDescription(String description){
-        this.description = description;
-    }
-
     public void setValue(BigDecimal value){
         this.value = value;
     }
 
-    public void setCategory(Category category){
-        this.category = category;
-    }
-
+    /**
+     * Representação textual da categoria
+     * @return representação textual
+     */
     @Override
     public String toString() {
         return "Product{" +
