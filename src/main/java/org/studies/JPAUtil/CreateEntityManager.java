d1027e65e734af44ba28045a0f1ed7fd16ba0936
my-github-2024 <carneiroangelojoaopedro@gmail.com>
2024-02-14 14:56:04 -0300

    Documentação da util

--- src/main/java/org/studies/JPAUtil/CreateEntityManager.java
+++ src/main/java/org/studies/JPAUtil/CreateEntityManager.java
@@ -4,11 +4,21 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+/**
+ * Classe Utilitária para criar um entityManager
+ *
+ * @author carneiroangelojoaopedro@gmail.com
+ */
 public class CreateEntityManager {
 
+    // FACTORY
     private static final EntityManagerFactory FACTORY = Persistence
             .createEntityManagerFactory("StudiesJPA");
 
+    /**
+     * Cria a entityManager
+     * @return instância de EntityManager
+     */
     public static EntityManager createEntityManager(){
         return FACTORY.createEntityManager();
     }
