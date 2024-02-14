710b52453d2ca668137eb36787b4affcdcad7e4a
my-github-2024 <carneiroangelojoaopedro@gmail.com>
2024-02-14 14:55:01 -0300

    Documentação da Exception

--- src/main/java/org/studies/exception/RegraDeNegocioException.java
+++ src/main/java/org/studies/exception/RegraDeNegocioException.java
@@ -1,7 +1,17 @@
 package org.studies.exception;
 
+/**
+ * Exceção - RegraDeNegócio
+ * Invocada quando alguma regra de negócio for quebrada
+ *
+ * @author carneiroangelojoaopedro@gmail.com
+ */
 public class RegraDeNegocioException extends RuntimeException {
 
+    /**
+     * Construtor
+     * @param msg Mensagem de erro
+     */
     public RegraDeNegocioException(String msg){
         super(msg);
     }
